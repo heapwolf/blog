61dcb1c91d7253bc6cc1db182cb16c42f6780c9a
Paolo Fragomeni <paolo@async.ly>
2012-10-14 15:05:46 +0200

    [app] wtf tabs

--- server/index.js
+++ server/index.js
@@ -30,47 +30,47 @@ file = file.replace('<content/>', content.join('<br/><hr><br/>'));
 
 http.createServer(function (req, res) {
 
-	if (req.url === '/' || req.url === '/index.html') {
-		res.statusCode = 200;
-		res.writeHeader('Content-Type', 'test/html');
-	  res.end(file);
-	  return;
-	}
+  if (req.url === '/' || req.url === '/index.html') {
+    res.statusCode = 200;
+    res.writeHeader('Content-Type', 'test/html');
+    res.end(file);
+    return;
+  }
 
-	//
-	// figure out what's in the request.
-	//
-	var rawurl = url.parse(req.url);
-	var pathname = decodeURI(rawurl.pathname);
-	var base = path.join(__dirname, '..', 'public');
+  //
+  // figure out what's in the request.
+  //
+  var rawurl = url.parse(req.url);
+  var pathname = decodeURI(rawurl.pathname);
+  var base = path.join(__dirname, '..', 'public');
   var filepath = path.normalize(path.join(base, pathname));
 
   //
   // set the appropriate mime type if possible.
   //
-	var mimetype = mime.lookup(path.extname(filepath).split(".")[1]);
-	
-	if (!mimetype) {
-		return;
-	}
+  var mimetype = mime.lookup(path.extname(filepath).split(".")[1]);
+  
+  if (!mimetype) {
+    return;
+  }
 
-	res.writeHeader('Content-Type', mimetype);
+  res.writeHeader('Content-Type', mimetype);
 
-	//
-	// find out if the file is there and if it is serve it...
-	//
+  //
+  // find out if the file is there and if it is serve it...
+  //
   fs.stat(filepath, function (err, stat) {
-  	
+    
     if (err && err.code === 'ENOENT') {
-			  res.statusCode = 404;
-			  res.end('not found');
+        res.statusCode = 404;
+        res.end('not found');
     }
     else {
-			if (!stat.isDirectory()) {
-			  res.statusCode = 200;
-			  fs.createReadStream(filepath).pipe(res);
-			}
-		}
+      if (!stat.isDirectory()) {
+        res.statusCode = 200;
+        fs.createReadStream(filepath).pipe(res);
+      }
+    }
   });
 
 }).listen(8080);
